031b2c7cffba9698cb281eeae5353a2ebae3e6f1
arty gvozdenkov <gvozdenkov@gmail.com>
2025-02-05 10:48:26 +0300

    chore: add prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -7,6 +7,5 @@ export default {
   rules: {
     'custom-property-pattern': '_?.+',
     'selector-class-pattern': '^([a-z][a-z0-9]*)((__|_|-|--)[a-z0-9]+)*$',
-    'prettier/prettier': [true, { singleQuote: true }],
   },
 };
